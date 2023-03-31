3dd83a6c7908d60bc9e848ba82c00baa98e60d47
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2023-03-31 10:51:11 +0200

    Add initial implementation of random preferences and preference loading

--- communication/preferences/Preferences.py
+++ communication/preferences/Preferences.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+from typing import Dict, List
+import random
+import math
+import pandas as pd
+
 from .CriterionName import CriterionName
 from .CriterionValue import CriterionValue
 from .Item import Item
 from .Value import Value
 
-import random
-import math
 
 class Preferences:
     """Preferences class.
@@ -21,6 +24,45 @@ class Preferences:
         """Creates a new Preferences object."""
         self.__criterion_name_list: list[CriterionName] = []
         self.__criterion_value_list: list[CriterionValue] = []
+
+    @staticmethod
+    def generate_random(items: List[Item]):
+        # Generate a random criterion name list
+        p = Preferences()
+        criterion_name_list = list(CriterionName)
+        random.shuffle(criterion_name_list)
+        p.set_criterion_name_list(criterion_name_list)
+
+        # Generate a random value for each criterion
+        for item in items:
+            for criterion_name in CriterionName:
+                # Random value for each item and criterion
+                value = random.choice(list(Value))
+                p.add_criterion_value(CriterionValue(item, criterion_name, value))
+
+        return p
+
+    def read_values_from_csv(
+        self, filepath: str, items: Dict[str, Item] = {}
+    ) -> Dict[str, Item]:
+        data = pd.read_csv(filepath)
+
+        # Create any items which are missing in the items dictionary
+        for item_name in data["item"].unique():
+            if item_name not in items:
+                items[item_name] = Item(item_name, "")
+
+        # Go through each row and add the corresponding criterion value
+        for _, row in data.iterrows():
+            # Parse this row's data
+            item = items[row["item"]]
+            criterion_name = CriterionName[row["criterion_name"]]
+            value = Value[row["value"]]
+
+            # Add it to the preferences
+            self.add_criterion_value(CriterionValue(item, criterion_name, value))
+
+        return items
 
     def get_criterion_name_list(self):
         """Returns the list of criterion name."""
@@ -73,7 +115,7 @@ class Preferences:
 
         # select randomly among the best scores
         selected_item = random.choice(best_scores)[1]
-        
+
         return selected_item
 
     def is_item_among_top_10_percent(self, item: Item, item_list: list[Item]) -> bool:
@@ -87,12 +129,13 @@ class Preferences:
         # take the ceil of the resulting division
         k_top10 = math.ceil(len(item_list) / 10)
 
-        item_list.sort(key= lambda item : item.get_score(self), reverse=True)
-        
+        item_list.sort(key=lambda item: item.get_score(self), reverse=True)
+
         if item in item_list[:k_top10]:
             return True
-        
+
         return False
+
 
 if __name__ == "__main__":
     """Testing the Preferences class."""
@@ -178,7 +221,42 @@ if __name__ == "__main__":
 
     print(
         "Diesel is in top 10 percent : {}".format(
-            agent_pref.is_item_among_top_10_percent(diesel_engine, [diesel_engine, electric_engine])
+            agent_pref.is_item_among_top_10_percent(
+                diesel_engine, [diesel_engine, electric_engine]
+            )
         )
     )
 
+    # Test reading preferences from csv for agent 1
+    p = Preferences()
+    p.set_criterion_name_list(
+        [
+            CriterionName.PRODUCTION_COST,  # c1
+            CriterionName.ENVIRONMENT_IMPACT,  # c4
+            CriterionName.CONSUMPTION,  # c2
+            CriterionName.DURABILITY,  # c3
+            CriterionName.NOISE,  # c5
+        ]
+    )
+    items = {"ICED": diesel_engine}
+    items = p.read_values_from_csv("data/agent_1.csv", items)
+    assert items["ICED"] == diesel_engine, "Existing items are not changed"
+    assert "E" in items, "Missing items are added"
+    assert p.get_value(items["ICED"], CriterionName.CONSUMPTION) == Value.AVERAGE
+    assert p.get_value(items["E"], CriterionName.ENVIRONMENT_IMPACT) == Value.GOOD
+
+    # Test reading preferences from csv for agent 2
+    p = Preferences()
+    p.set_criterion_name_list(
+        [
+            CriterionName.ENVIRONMENT_IMPACT,  # c4
+            CriterionName.NOISE,  # c5
+            CriterionName.PRODUCTION_COST,  # c1
+            CriterionName.CONSUMPTION,  # c2
+            CriterionName.DURABILITY,  # c3
+        ]
+    )
+    items = p.read_values_from_csv("data/agent_2.csv", items)
+    assert p.get_value(items["ICED"], CriterionName.CONSUMPTION) == Value.BAD
+    assert p.get_value(items["E"], CriterionName.ENVIRONMENT_IMPACT) == Value.GOOD
+    print("read_value_from_csv OK")
