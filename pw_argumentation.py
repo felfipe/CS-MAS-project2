7502f1b273d9abb53337f98bbe60674617e049b4
Victor Amaral <victoramaral@usp.br>
2023-04-14 11:21:18 +0200

    adapt code to one loop

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -27,12 +27,12 @@ class ArgumentModel(Model):
             Item("E", "A very quiet engine"),
         ]
 
-        self.agent_1 = self._create_agent("Alice", prefs_agent_1)
-        self.agent_2 = self._create_agent("Bob", prefs_agent_2)
+        self.agent_1 = self._create_agent("Alice", prefs_agent_1, True)
+        self.agent_2 = self._create_agent("Bob", prefs_agent_2, False)
 
         self.running = True
 
-    def _create_agent(self, name: str, prefs_path: Optional[str]):
+    def _create_agent(self, name: str, prefs_path: Optional[str], initial_agent : bool):
         # Load or generate the preferences depending on if the prefs_path is set
         if prefs_path is None:
             prefs = Preferences.generate_random(self.items)
@@ -40,7 +40,7 @@ class ArgumentModel(Model):
             prefs = Preferences.load(prefs_path, self.items)
 
         # Create the agent and add it to the scheduler
-        agent = ArgumentAgent(self.next_id(), self, name, prefs)
+        agent = ArgumentAgent(self.next_id(), self, name, prefs, initial_agent)
         self.scheduler.add(agent)
 
         return agent
@@ -65,12 +65,17 @@ class ArgumentAgent(CommunicatingAgent):
         model: ArgumentModel,
         name: str,
         preferences: Preferences,
+        initial_agent: bool
     ):
         super().__init__(unique_id, model, name)
-        self.init_prop = False
         self.preferences = preferences
         self.items = self.model.items.copy()
+
+        self.initial_agent = initial_agent
         self.proposed_item = self.items[1]
+
+        self.has_committed = False
+        self.has_proposed = False
 
     def step(self):
         super().step()
@@ -79,8 +84,8 @@ class ArgumentAgent(CommunicatingAgent):
         if len(messages) > 0:
             print(*messages, sep="\n")
 
-        if self.get_name() == "Alice" and not self.init_prop:
-            self.init_prop = True
+        if self.initial_agent and not self.has_proposed:
+            self.has_proposed = True
             self.send_message(
                 Message(
                     self.get_name(),
@@ -91,45 +96,19 @@ class ArgumentAgent(CommunicatingAgent):
             )
             return
 
-        if self.get_name() == "Alice":
-            for msg in messages:
-                if (
-                    msg.get_performative() == MessagePerformative.ACCEPT
-                    and msg.get_content() == self.proposed_item
-                ):
-                    self.send_message(
-                        Message(
-                            self.get_name(),
-                            None,
-                            MessagePerformative.COMMIT,
-                            self.proposed_item,
-                        )
-                    )
-                elif (
-                    msg.get_performative() == MessagePerformative.COMMIT
-                    and msg.get_content() == self.proposed_item
-                ):
-                    self.items.remove(self.proposed_item)
-                else:
-                    self.send_message(
-                        Message(self.get_name(), None, MessagePerformative.ARGUE, None)
-                    )
+        for msg in messages:
 
-            return
-
-        if self.get_name() == "Bob":
-            for msg in messages:
-                if msg.get_performative() == MessagePerformative.PROPOSE:
-                    self.received_proposal = msg.get_content()
+            if msg.get_performative() == MessagePerformative.PROPOSE:
+                    self.received_proposition = msg.get_content()
                     if self.preferences.is_item_among_top_10_percent(
-                        self.proposed_item, self.items
+                        self.received_proposition, self.items
                     ):
                         self.send_message(
                             Message(
                                 self.get_name(),
                                 msg.get_exp(),
                                 MessagePerformative.ACCEPT,
-                                self.proposed_item,
+                                self.received_proposition,
                             )
                         )
                     else:
@@ -138,27 +117,45 @@ class ArgumentAgent(CommunicatingAgent):
                                 self.get_name(),
                                 msg.get_exp(),
                                 MessagePerformative.ASK_WHY,
-                                self.proposed_item,
+                                self.received_proposition,
                             )
                         )
-                elif (
-                    msg.get_performative() == MessagePerformative.COMMIT
-                    and msg.get_content() == self.received_proposal
-                ):
-                    self.items.remove(self.received_proposal)
+            elif (
+                msg.get_performative() == MessagePerformative.ACCEPT
+                and msg.get_content() == self.proposed_item
+            ):
+                self.send_message(
+                    Message(
+                        self.get_name(),
+                        None,
+                        MessagePerformative.COMMIT,
+                        self.proposed_item,
+                    )
+                )
+                self.has_committed = True
+                
+            elif (
+                msg.get_performative() == MessagePerformative.COMMIT
+            ):
+                if(msg.get_content() == self.proposed_item and self.has_committed):
+                    self.items.remove(self.proposed_item)
+                elif(msg.get_content() == self.received_proposition):
+                    self.items.remove(self.received_proposition)
                     self.send_message(
                         Message(
                             self.get_name(),
                             None,
                             MessagePerformative.COMMIT,
-                            self.received_proposal,
+                            self.received_proposition,
                         )
                     )
-                else:
-                    self.send_message(
-                        Message(self.get_name(), None, MessagePerformative.ARGUE, None)
-                    )
-                return
+                    self.has_committed = True
+            else:
+                self.send_message(
+                    Message(self.get_name(), None, MessagePerformative.ARGUE, None)
+                )
+
+            return
 
     def get_preferences(self):
         return self.preferences
